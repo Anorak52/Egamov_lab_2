33076c5acfb9c851ad244d2651ee911682a39d18
Anorak52 <siber56@yandex.ru>
2020-05-18 21:48:29 +0300

    Initial release

---
+++ wm_lab2_nummethd.py
@@ -0,0 +1,90 @@
+import wm_lab_calc
+import matplotlib
+matplotlib.use('TkAgg')
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from tkinter import *
+import tkinter.ttk as ttk
+import tkinter as tk
+import time, threading
+import math
+from matplotlib import pyplot as plt
+
+main = Tk()
+main.title("Численное методы lab2")
+main.geometry("400x350")
+
+label1 = Label(text = "Длина стержня", font = "Arial 11")
+label1.grid(row=0, column=0)
+label2 = Label(text = "Время воздействия", font = "Arial 11")
+label2.grid(row=1, column=0)
+label3 = Label(text = "Шаг по t ", font = "Arial 11")
+label3.grid(row=6, column=0)
+label4 = Label(text = "Шаг по x ", font = "Arial 11")
+label4.grid(row=5, column=0)
+label5 = Label(text = "b₀ = ", font = "Arial 11")
+label5.grid(row=3, column=0)
+label6 = Label(text = "b1 = ", font = "Arial 11")
+label6.grid(row=8, column=0)
+label7 = Label(text = "b2 = ", font = "Arial 11")
+label7.grid(row=9, column=0)
+label8 = Label(text = "Фи0 = ", font = "Arial 11")
+label8.grid(row=2, column=0)
+label9 = Label(text = "Фи1 = ", font = "Arial 11")
+label9.grid(row=7, column=0)
+
+_l=DoubleVar()
+_t=DoubleVar()
+_h1=DoubleVar()
+_h2=DoubleVar()
+_b0=DoubleVar()
+_b1=DoubleVar()
+_b2=DoubleVar()
+_f0=DoubleVar()
+_f1=DoubleVar()
+
+entry1 = Entry(textvariable = _l, font = "Arial 11")
+entry1.grid(row=0, column=1, padx=5, pady=5)
+entry2 = Entry(textvariable = _t, font = "Arial 11")
+entry2.grid(row=1, column=1, padx=5, pady=5)
+entry3 = Entry(textvariable = _h1, font = "Arial 11")
+entry3.grid(row=6, column=1, padx=5, pady=5)
+entry4 = Entry(textvariable = _h2, font = "Arial 11")
+entry4.grid(row=5, column=1, padx=5, pady=5)
+entry5 = Entry(textvariable = _b0, font = "Arial 11")
+entry5.grid(row=3, column=1, padx=5, pady=5)
+entry6 = Entry(textvariable = _b1, font = "Arial 11")
+entry6.grid(row=8, column=1, padx=5, pady=5)
+entry7 = Entry(textvariable = _b2, font = "Arial 11")
+entry7.grid(row=9, column=1, padx=5, pady=5)
+entry8 = Entry(textvariable = _f0, font = "Arial 11")
+entry8.grid(row=2, column=1, padx=5, pady=5)
+entry9 = Entry(textvariable = _f1, font = "Arial 11")
+entry9.grid(row=7, column=1, padx=5, pady=5)
+
+entry1.delete(0, END)
+entry1.insert(0, "20.0")
+entry2.delete(0, END)
+entry2.insert(0, "10.0")
+entry3.delete(0, END)
+entry3.insert(0, "0.01")
+entry4.delete(0, END)
+entry4.insert(0, "0.2")
+entry5.delete(0, END)
+entry5.insert(0, "0.001")
+entry6.delete(0, END)
+entry6.insert(0, "0.03")
+entry7.delete(0, END)
+entry7.insert(0, "0.0")
+entry8.delete(0, END)
+entry8.insert(0, "0.0")
+entry9.delete(0, END)
+entry9.insert(0, "0.0")
+
+btn = Button(main, text= "График", command = lambda: wm_lab_calc.main(float(_l.get()), 
+float(_t.get()), float(_h1.get()), float(_h2.get()), float(_b0.get()), float(_b1.get()), 
+float(_b2.get()), float(_f0.get()), float(_f1.get())))
+btn.grid(row=10,column=0)
+
+main.mainloop()
---
+++ wm_lab_calc.py
@@ -0,0 +1,150 @@
+#Copyright Brazhnikov Eugene 2020
+
+import matplotlib
+matplotlib.use('TkAgg')
+import numpy as np
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from tkinter import *
+import tkinter.ttk as ttk
+import tkinter as tk
+import time, threading
+import math
+from matplotlib import pyplot as plt
+
+def method(A, B, C, F):
+    size = len(F)
+    sizeM = size - 1
+    i = 1
+    a = [1] * sizeM
+    b = [1] * sizeM
+    x = [1] * size
+    a[0] = -C[0] / B[0]
+    b[0] = F[0] / B[0]
+    while (i < sizeM):
+        a[i] = -C[i] / (A[i] * a[i - 1] + B[i])
+        b[i] = (F[i] - A[i] * b[i - 1]) / (A[i] * a[i - 1] + B[i])
+        i = i + 1
+
+    x[sizeM] = (F[sizeM] - A[sizeM] * b[sizeM - 1]) / (B[sizeM] + A[sizeM] * a[sizeM - 1])
+    i = size - 2
+
+
+    while (i > -1):
+        x[i] = a[i] * x[i + 1] + b[i]
+        i = i - 1
+    return x 
+
+def main(l, T, h_t, h_x, b0, b1, b2, f0, f1):
+    
+    max = int(2 * T / h_t)
+    n = int(l / h_x) + 1
+    m = int(T / h_t)
+    A = [0] * n
+    B = [0] * n
+    C = [0] * n
+    f_ = [0] * n
+    w= [0] * n
+    b = [0] * n
+    f = [0] * n
+    x = [0] * n
+    j = 0
+
+
+    while (j < m):
+        i = 0
+        while (i < n):
+            if (j == 0):
+                x[i] = i * h_x
+                f[i] = 1 / l + f0 * math.cos(math.pi * x[i] / l) + f1 * math.cos(2 * math.pi * x[i] / l)
+                f_[i] = f[i]
+                b[i] = b0 + b1 * math.cos(math.pi * x[i] / l) + b2 * math.cos(2 * math.pi * x[i] / l)
+            else:
+                f_[i] = w[i]
+            B[i] = 1 + 2 * h_t / (h_x * h_x) - h_t * b[i] 
+            C[i] = -h_t / (h_x * h_x)
+            A[i] = -h_t / (h_x * h_x)
+            i = i + 1
+        A[0] = 0
+        B[0] = 1 + h_t / (h_x * h_x) - h_t * b[0]
+        C[0] = -h_t / (h_x * h_x)
+        A[n - 1] = -h_t / (h_x * h_x)
+        B[n - 1] = 1 + h_t / (h_x * h_x) - h_t * b[n - 1]
+        C[n - 1] = 0
+        w = method(A, B, C, f_)
+        j = j + 1
+    w_I = w[0] + w[n-1]
+    j = 1
+
+
+    while(j < n - 1):
+        if (j%2 == 1):
+            w_I = w_I + 4 * w[j]
+        else:
+            w_I = w_I + 2 * w[j]
+        j = j + 1
+    w_I = w_I * h_x / 6
+    for i in range(n):
+        w[i] = w[i] / w_I
+    j = 0
+    y = [0] * n
+    I = 0
+
+
+    while (j < m):
+        i = 0
+        while (i < n):
+            if (j == 0):
+                f[i] = 1 / l + f0 * math.cos(math.pi * x[i] / l) + f1 * math.cos(2 * math.pi * x[i] / l)
+                f_[i] = f[i]
+                b[i] = b0 + b1 * math.cos(math.pi * x[i] / l) + b2 * math.cos(2 * math.pi * x[i] / l)
+            else:
+                    f_[i] = y[i]
+            B[i] = 1 + 2 * h_t / (h_x * h_x) - h_t * b[i] + h_t * I
+            C[i] = -h_t / (h_x * h_x)
+            A[i] = -h_t / (h_x * h_x)
+            i = i + 1
+        A[0] = 0
+        B[0] = 1 + h_t / (h_x * h_x) - h_t * b[0] + h_t* I
+        C[0] = -h_t / (h_x * h_x)
+        A[n - 1] = -h_t / (h_x * h_x)
+        B[n - 1] = 1 + h_t / (h_x * h_x) - h_t * b[n - 1] + h_t * I
+        C[n - 1] = 0
+        y = method(A, B, C, f_)
+        I = y[0] * b[0]+ y[n - 1] * b[n - 1]
+        k = 1
+        while (k < n - 1):
+            if (k%2 == 1):
+                I = I + 4 * y[k] * b[k]
+            else:
+                I = I + 2 * y[k] * b[k]
+            k = k + 1
+        I = I * h_x / 6
+        j = j + 1
+
+
+    global green, _x, a, canvas
+    green = w
+    _x = x
+    fig = Figure(figsize=(8,5))
+    a = fig.add_subplot(111)
+    a.plot(x, f, label = "Start temp", color='blue')
+    a.plot(x, y, label = "Fin temp", color='red')
+    a.set_ylabel("t", fontsize=10)
+    a.set_xlabel("x", fontsize=10)
+    a.legend()
+    root = Tk()
+    root.title("Graphic")
+    root.geometry("900x700")
+    
+    canvas = FigureCanvasTkAgg(fig, master=root)
+    canvas.get_tk_widget().place(relx=0.1, rely=0.1) #сдвиг по окну
+    canvas.draw()
+    btn = Button(root, text= "Построить", font = "Arial 10", command = continue_)
+    btn.grid(row=0,column=0)
+
+
+def continue_():
+    a.plot(_x, green, label = "Final temp A", color='green')
+    a.legend()
+    canvas.draw()
